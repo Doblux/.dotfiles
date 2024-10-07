a878c3d6045577f7969728a6b025479e53991794
Doblux <dobluxGithub@protonmail.com>
2024-10-07 08:48:40 -0300

    new nvim config

--- .config/nvim/lua/doblux/lazy/lualine.lua
+++ .config/nvim/lua/doblux/lazy/lualine.lua
@@ -1,10 +1,16 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  config = function ()
-    require('lualine').setup({
-      options = {
-        theme = 'auto'
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local custom_gruvbox = require("lualine.themes.auto")
+		-- Change the background of lualine_c section for normal mode
+		local auto_theme = require("lualine.themes.auto")
+		custom_gruvbox.normal.c.bg = "#112233"
+		require("lualine").setup({
+			icons_enabled = true,
+			options = { theme = auto_theme },
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+		})
+	end,
 }
--- .config/nvim/lua/doblux/set.lua
+++ .config/nvim/lua/doblux/set.lua
@@ -3,34 +3,29 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+vim.opt.tabstop = 2 -- 2 espacios = 1 tab
+vim.opt.softtabstop = 2 -- 2 espacios = 1 tab en modo insertar
+vim.opt.shiftwidth = 2 -- cantidad de espacios para la indentacion al mover lineas de codigo
+vim.opt.expandtab = true -- Cuando se presiona la tecla de tabulación, se insertarán 4 espacios en lugar de un solo tabulador.
 
-vim.opt.smartindent = true
+vim.opt.smartindent = true -- activa la indentación inteligente, el editor ajustará automaticamente la indentacion segun el contexto
 
-vim.opt.wrap = false
+vim.opt.wrap = false -- 1 linea larguisima en lugar de varias lineas que pertenecen al mismo renglon
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undofile = true
+vim.opt.swapfile = false -- Esta opción desactiva la creación de archivos de intercambio (swap files).
+vim.opt.backup = false -- Neovim no creará estos archivos de respaldo
+-- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- vim.opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+vim.opt.hlsearch = false -- Esta opción desactiva el resaltado de las búsquedas realizadas.
+vim.opt.incsearch = true -- Esta opción activa la búsqueda incremental (mientras busco se va resaltando)
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.scrolloff = 8 -- Esta opción define el número de líneas que se mantendrán visibles por encima y por debajo del cursor cuando te desplazas por el texto
+vim.opt.signcolumn = "yes" -- Esta opción activa la columna de signos, que muestra indicadores para errores, advertencias y otros signos que puedas configurar en tu archivo.
+vim.opt.isfname:append("@-@") -- añade el carácter @ al conjunto de caracteres que Neovim considera válidos para nombres de archivo.
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 50 --  Esta opción determina el tiempo, en milisegundos, que Neovim espera antes de actualizar ciertos eventos, como el autoguardado y la visualización de cambios.
 
-vim.opt.colorcolumn = "180"
-
-vim.g.c_syntax_for_h = 1
-
-vim.api.nvim_exec([[
-  autocmd BufNewFile,BufRead *.c setfiletype c
-]], false)
+vim.opt.cmdheight = 0
